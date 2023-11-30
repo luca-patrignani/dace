40d61c5b01c7e5b8fd9a876b69c76aeec18aa8f8
Lukas Truemper <lukas.truemper@outlook.de>
2023-11-30 15:40:07 +0100

    Symmetric change in PruneConnectors

--- dace/transformation/dataflow/prune_connectors.py
+++ dace/transformation/dataflow/prune_connectors.py
@@ -1,6 +1,7 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 from typing import Set, Tuple
 import re
+import networkx as nx
 
 from dace import dtypes, SDFG, SDFGState, symbolic, properties, data as dt
 from dace.transformation import transformation as pm, helpers
@@ -57,39 +58,37 @@ class PruneConnectors(pm.SingleStateTransformation):
         nsdfg = self.nsdfg
 
         # Fission subgraph around nsdfg into its own state to avoid data races
-        predecessors = set()
-        for inedge in state.in_edges(nsdfg):
-            if inedge.data is None:
-                continue
+        all_nodes = None
+        for cc in nx.weakly_connected_components(state._nx):
+            if nsdfg in cc:
+                all_nodes = set(cc)
 
-            pred = state.memlet_path(inedge)[0].src
-            if state.in_degree(pred) == 0:
-                continue
-
-            predecessors.add(pred)
-            for e in state.bfs_edges(pred, reverse=True):
-                predecessors.add(e.src)
-
-        subgraph = StateSubgraphView(state, predecessors)
-        pred_state = helpers.state_fission(sdfg, subgraph)
-
-        subgraph_nodes = set()
-        subgraph_nodes.add(nsdfg)
+        input_nodes = set()
+        nsdfg_nodes = set()
+        nsdfg_nodes.add(nsdfg)
         for inedge in state.in_edges(nsdfg):
             if inedge.data is None:
                 continue
             path = state.memlet_path(inedge)
             for edge in path:
-                subgraph_nodes.add(edge.src)
+                nsdfg_nodes.add(edge.src)
+
+            if state.in_degree(path[0].src) > 0:
+                input_nodes.add(path[0].src)
 
         for oedge in state.out_edges(nsdfg):
             if oedge.data is None:
                 continue
             path = state.memlet_path(oedge)
             for edge in path:
-                subgraph_nodes.add(edge.dst)
+                nsdfg_nodes.add(edge.dst)
 
-        subgraph = StateSubgraphView(state, subgraph_nodes)
+        successors = set([edge.dst for edge in state.bfs_edges(nsdfg)])
+        predecessors = (all_nodes - successors - nsdfg_nodes) | input_nodes
+        subgraph = StateSubgraphView(state, predecessors)
+        _ = helpers.state_fission(sdfg, subgraph)
+
+        subgraph = StateSubgraphView(state, nsdfg_nodes)
         nsdfg_state = helpers.state_fission(sdfg, subgraph)
 
         read_set, write_set = nsdfg.sdfg.read_and_write_sets()
